26c9cd68715e915071e4945198b50182af95eb58
Unknown <Ivan4ik228@rambler.ru>
2017-10-01 11:50:58 +0300

    Some fixes

--- game.c
+++ game.c
@@ -39,11 +39,14 @@ char startGame(char array[15][8], char figure, char blc1hi, char blc2hi, char bl
        kek6 = blc2hi,
        kek7 = blc3hi,
        kek8 = blc4hi;
+  char blcwe1 = blc1we,
+       blcwe2 = blc2we,
+       blcwe3 = blc3we,
+       blcwe4 = blc4we;
   while(1) 
   {
     if (*mek == 1)
     {
-      shMatrix(8, 15, array, figure);
       if(array[kek1 + 1][blc1we] == 2 || array[kek2 + 1][blc2we] == 2 || array[kek3 + 1][blc3we] == 2 || array[kek4 + 1][blc4we] == 2 || kek5 + 1 == 15 || kek6 + 1 == 15 || kek7 + 1 == 15 || kek8 + 1 == 15)
         {
           for(char i = 0;i < 15;i++)
@@ -53,8 +56,55 @@ char startGame(char array[15][8], char figure, char blc1hi, char blc2hi, char bl
                   array[i][j] = 2;
               }
           }
+          for(char i = 0; i < 15; i++)
+          {
+            char count = 0;
+            for(char j = 0;j < 8;j++){
+              if (array[i][j] == 2) 
+              {
+                count++;
+                if(count == 8)
+                {
+                  for(char j = 0;j < 8;j++)
+                    array[i][j] = 0;
+                    
+                  for(char k = 15; k > 0; k++)
+                  {
+                    for(char h = 0; h < 8; h++)
+                    {
+                      char g = h;
+                      if(array[h][k] == 2)
+                      {
+                        array[h][k] = 0;
+                        if(h == 15)
+                          array[g + 1][k] = 2;
+                        else if(h == 14)
+                          array[g + 3][k] = 2;
+                        else if(h == 13)                   
+                          array[g + 5][k] = 2;
+                        else if(h == 12)                   
+                          array[g + 7][k] = 2;
+                        shMatrix(8, 15, array, figure);
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+          
+
+
+          for(char j = 0;j < 8;j++){
+            if (array[0][j] == 2) 
+              return 0;
+            else
+              return 1;
+          }
+
+          
+
           shMatrix(8, 15, array, figure);
-          return 1;
         } else
         {
           array[blc1hi][blc1we] = 0, array[blc2hi][blc2we] = 0, array[blc3hi][blc3we] = 0, array[blc4hi][blc4we] = 0;
@@ -171,23 +221,108 @@ char startGame(char array[15][8], char figure, char blc1hi, char blc2hi, char bl
             } 
                 break;
             case 'a':
-            if(blc1we - 1 < 0 || blc2we - 1 < 0 || blc3we - 1 < 0 || blc4we - 1 < 0)
+            blcwe1 = blc1we,
+            blcwe2 = blc2we,
+            blcwe3 = blc3we,
+            blcwe4 = blc4we;
+            if(blc1we - 1 < 0 || blc2we - 1 < 0 || blc3we - 1 < 0 || blc4we - 1 < 0 )
             {
-        
               shMatrix(8, 15, array, figure);
             } else {
-              array[blc1hi][blc1we] = 0, array[blc2hi][blc2we] = 0, array[blc3hi][blc3we] = 0, array[blc4hi][blc4we] = 0;
-              array[blc1hi][--blc1we] = 1, array[blc2hi][--blc2we] = 1, array[blc3hi][--blc3we] = 1, array[blc4hi][--blc4we] = 1;
-              shMatrix(8, 15, array, figure);
+              if(array[blc1hi][blcwe1 - 1] == 2 || array[blc2hi][blcwe2 - 1] == 2 || array[blc3hi][blcwe3 - 1] == 2 || array[blc4hi][blcwe4 - 1] == 2)
+              {
+                /* code */
+              } else{
+                array[blc1hi][blc1we] = 0, array[blc2hi][blc2we] = 0, array[blc3hi][blc3we] = 0, array[blc4hi][blc4we] = 0;
+                array[blc1hi][--blc1we] = 1, array[blc2hi][--blc2we] = 1, array[blc3hi][--blc3we] = 1, array[blc4hi][--blc4we] = 1;
+                shMatrix(8, 15, array, figure);
+              }
             }
             break;
             case 'd':
+            blcwe1 = blc1we,
+            blcwe2 = blc2we,
+            blcwe3 = blc3we,
+            blcwe4 = blc4we;
             if(blc1we + 2 > 8 || blc2we + 2 > 8 || blc3we + 2 > 8 || blc4we + 2 > 8 )
             {
               shMatrix(8, 15, array, figure);
             } else {
+              if(array[blc1hi][blcwe1 + 1] == 2 || array[blc2hi][blcwe2 + 1] == 2 || array[blc3hi][blcwe3 + 1] == 2 || array[blc4hi][blcwe4 + 1] == 2)
+              {
+                /* code */
+              } else{
+                array[blc1hi][blc1we] = 0, array[blc2hi][blc2we] = 0, array[blc3hi][blc3we] = 0, array[blc4hi][blc4we] = 0;
+                array[blc1hi][++blc1we] = 1, array[blc2hi][++blc2we] = 1, array[blc3hi][++blc3we] = 1, array[blc4hi][++blc4we] = 1;
+                shMatrix(8, 15, array, figure);
+              }
+            }
+            break;
+            case 's':
+            kek1 = blc1hi;
+            kek2 = blc2hi;
+            kek3 = blc3hi;
+            kek4 = blc4hi;
+            if(blc1hi + 2 > 15 || blc2hi + 2 > 15 || blc3hi + 2 > 15 || blc4hi + 2 > 15 || array[kek1 + 1][blc1we] == 2 || array[kek2 + 1][blc2we] == 2 || array[kek3 + 1][blc3we] == 2 || array[kek4 + 1][blc4we] == 2) {
+              for(char i = 0;i < 15;i++)
+              {
+                  for(char j = 0;j < 8;j++){
+                    if (array[i][j] == 1) 
+                      array[i][j] = 2;
+                  }
+              }
+              for(char i = 0; i < 15; i++)
+              {
+                char count = 0;
+                for(char j = 0;j < 8;j++){
+                  if (array[i][j] == 2) 
+                  {
+                    count++;
+                    if(count == 8)
+                    {
+                      for(char j = 0;j < 8;j++)
+                        array[i][j] = 0;
+                        
+                      for(char k = 15; k > 0; k++)
+                      {
+                        for(char h = 0; h < 8; h++)
+                        {
+                          char g = k;
+                          if(array[k][h] == 2)
+                          {
+                            array[k][h] = 0;
+                            if(k == 15)
+                              array[g - 1][h] = 2;
+                            else if(k == 14)
+                              array[g - 3][h] = 2;
+                            else if(k == 13)                   
+                              array[g - 5][h] = 2;
+                            else if(k == 12)                   
+                              array[g - 7][h] = 2;
+                            shMatrix(8, 15, array, figure);
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+              
+    
+    
+              for(char j = 0;j < 8;j++){
+                if (array[0][j] == 2) 
+                  return 0;
+                else
+                  return 1;
+              }
+    
+              
+    
+              shMatrix(8, 15, array, figure);
+            } else {
               array[blc1hi][blc1we] = 0, array[blc2hi][blc2we] = 0, array[blc3hi][blc3we] = 0, array[blc4hi][blc4we] = 0;
-              array[blc1hi][++blc1we] = 1, array[blc2hi][++blc2we] = 1, array[blc3hi][++blc3we] = 1, array[blc4hi][++blc4we] = 1;
+              array[++blc1hi][blc1we] = 1, array[++blc2hi][blc2we] = 1, array[++blc3hi][blc3we] = 1, array[++blc4hi][blc4we] = 1;
               shMatrix(8, 15, array, figure);
             }
             break;
@@ -195,10 +330,9 @@ char startGame(char array[15][8], char figure, char blc1hi, char blc2hi, char bl
           }
           
 
-      
+    
   }
-
-  }
+}
       
 
 
@@ -257,7 +391,7 @@ void shMatrix(char width, char height, char array[height][width], char figureNow
 
 
 
-
+// Еще не готово!
 
 
 void main(void)
@@ -273,7 +407,7 @@ void main(void)
     if (g == 1)
     {
       printf("New figure request\n");
-    } else printf("What?\n");
+    } else if (g == 0) return;
   }
   
 
